71c9bada1fc759033f0924cb4713d76a3908ef8d
Peyal Hasan <peyalhasan143@gmail.com>
2025-09-29 11:56:34 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,23 @@
+console.log('Type')
+// const drink = ‘    water ’;
+// const liquid = ‘water’;
+// if (drink.trim() === liquid.trim()) {
+//     console.log(“Safe Water”);
+// }
+// else {
+//     console.log(“Drink”)
+// }
+
+const drink = '   water';
+const liquid = 'water';
+if (drink.trim() === liquid.trim()) {
+    console.log('Are beta kha morti nah')
+}
+else {
+    console.log('Ek pack mar, Morle tor gf deikha rakhbo somossa nai')
+}
+console.log(drink.trim())
+
+const capital = 'Dha ka'
+console.log(capital.length)
+console.log(capital[3])
