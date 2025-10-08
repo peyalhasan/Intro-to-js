a356532dafdd309112b69847a13a103c3c5b69ad
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-08 13:20:02 +0600

    Function in js

---
+++ function.js
@@ -0,0 +1,22 @@
+
+function even(num) {
+    let evens = []
+    for (let i = 0; i <= num; i++) {
+        if (i % 2 === 0) {
+            evens.push(i)
+        }
+    }
+    return evens
+}
+console.log(even(10))
+
+function odd(num) {
+    let odds = []
+    for (let i = 0; i <= num; i++) {
+        if (i % 2 !== 0) {
+            odds.push(i)
+        }
+    }
+    return odds
+}
+console.log(odd(10))
